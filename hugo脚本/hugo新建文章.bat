3d927404333172f02eaec8a8742b7e5bcf007784
meimolihan <meimolihan@gmail.com>
2025-02-19 08:48:46 +0800

    update

--- hugo脚本/hugo新建文章.bat
+++ hugo脚本/hugo新建文章.bat
@@ -6,7 +6,7 @@ set "hugoDir=%USERPROFILE%\Desktop\GitHub\hugo-main"
 if not exist "%hugoDir%" (
     echo 目标目录 %hugoDir% 不存在，请检查路径是否正确。
     pause
-    exit /b
+    exit
 )
 
 :: 提示用户输入文件名
@@ -16,7 +16,7 @@ set /p fileName=请输入文章名：
 if "%fileName%"=="" (
     echo 文件名不能为空，请重新运行脚本并输入有效的文件名。
     pause
-    exit /b
+    exit
 )
 
 :: 切换到 Hugo 项目目录
@@ -31,15 +31,13 @@ set "contentPath=%hugoDir%\content\post\%fileName%\index.md"
 if not exist "%contentPath%" (
     echo 文件 %fileName% 的创建失败，请检查 Hugo 是否正确安装并配置。
     pause
-    exit /b
+    exit
 )
 
 :: 提示用户文件已创建
 echo 文件 %fileName% 已成功创建在 %contentPath%！
 
-:: 打开记事本并加载文件
+:: 打开记事本并加载文件后退出CMD
 echo 正在打开记事本...
 start notepad.exe "%contentPath%"
-
-:: 暂停脚本以便用户查看提示
-pause
+exit
